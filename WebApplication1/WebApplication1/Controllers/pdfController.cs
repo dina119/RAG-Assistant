a7b9b4cbc81075e9752c1cee4cdbf5d392de8bbb
dina119 <dinam5862@gmail.com>
2025-07-01 15:29:20 +0300

    upload File end point

---
+++ WebApplication1/WebApplication1/Controllers/pdfController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using UglyToad.PdfPig;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class pdfController : ControllerBase
+    {
+
+[HttpPost("extract-text")]
+        public IActionResult ExtractTextFromPdf([FromForm] Files File)
+        {
+            if (File.File == null || File.File.Length == 0)
+                return BadRequest("No file uploaded");
+
+            try
+            {
+                using var stream = File.File.OpenReadStream();
+                using var pdf = PdfDocument.Open(stream);
+                var text = string.Join("\n", pdf.GetPages().Select(p => p.Text));
+
+                return Ok(text);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error reading PDF: {ex.Message}");
+            }
+        }
+    
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Files.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.Models
+{
+    public class Files
+    {
+        public IFormFile File {  get; set; }
+    }
+}
